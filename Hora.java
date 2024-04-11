2860444eea3203ddad9f5aab3d6f545ae9587fa8
NTfabricio2000 <166646229+NTfabricio2000@users.noreply.github.com>
2024-04-11 17:53:53 -0300

    Hora.java

--- Hora.java
+++ Hora.java
@@ -1,29 +1,31 @@
-public class Hora {
-	private int hora;
-	private int minuto;
-	
-	public Hora(int h, int m) {
-		this.hora = h;
-		this.minuto = m;
+public class Horario {
+
+		private int hora;
+		private int minuto;
+		
+		public Horario(int h, int m) {
+			this.hora = h;
+			this.minuto = m;
+		}
+		
+		public void setHora(int novaHora) {
+			this.hora = novaHora;
+		}
+		
+		public void setMinuto(int novoMinuto) {
+			this.minuto = novoMinuto;
+		}
+		
+		public int getHora() {
+			return hora;
+		}
+		
+		public int getMinuto() {
+			return minuto;
+		}
+		
+		public String printlmHorario() {
+			return hora + ":" + minuto;
+		}
 	}
-	
-	public void setHora(int novaHora) {
-		this.hora = novaHora;
-	}
-	
-	public void setMinuto(int novoMinuto) {
-		this.minuto = novoMinuto;
-	}
-	
-	public int getHora() {
-		return hora;
-	}
-	
-	public int getMinuto() {
-		return minuto;
-	}
-	
-	public String imprimirHora() {
-		return hora + ":" + minuto;
-	}
-}
+
