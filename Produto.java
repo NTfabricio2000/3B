74c48acf853d40a12d078796379f47ac119ee16a
NTfabricio2000 <166646229+NTfabricio2000@users.noreply.github.com>
2024-04-17 16:38:04 -0300

    Produto.java

---
+++ Produto.java
@@ -0,0 +1,39 @@
+public class Produto {
+	private String Nome;
+	private double Preco;
+	private int Quantidade;
+
+private Produto(String n, double p, int q) {
+	this.Nome = n;
+	this.Preco = p;
+	this.Quantidade = q;
+}
+
+public String getNome() {
+	return Nome;
+}
+
+public double getPreco() {
+	return Preco;
+}
+	
+public int getQuantidade() {
+	return Quantidade;
+}
+
+public void setNome(String n) {
+	this.Nome = n;
+}
+
+public void setPreco(double p) {
+	this.Preco = p;
+}
+	
+public void setQuantidade(int q) {
+	this.Quantidade = q;
+}
+
+	public int multiplicar(double p, int q) {
+		return (Preco*Quantidade);
+	}
+}
