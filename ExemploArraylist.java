da477cd654a26c406fd079c26a266baad8be5374
NTfabricio2000 <166646229+NTfabricio2000@users.noreply.github.com>
2024-04-29 17:53:09 -0300

    ExemploArraylist.java

---
+++ ExemploArraylist.java
@@ -0,0 +1,39 @@
+public class ExemploArraylist {
+     public static void main(String[] args) {
+    	 ArrayList<String> nome = new ArrayList<String>();
+    	 
+    	 nome.add("Fabricio");
+    	 nome.add("Malricio");
+    	 nome.add("Miriam");
+    	 nome.add("Paulo");
+    	 nome.add("Gorran");
+    	 
+    	 System.out.println(nome);
+     
+
+
+ 
+      ArrayList<Integer> nomedois = new ArrayList<Integer>();
+        	 
+        	 nomedois.add(65);
+        	 nomedois.add(78);
+        	 nomedois.add(22);
+        	 nomedois.add(55);
+        	 nomedois.add(22);
+        	 
+        	 System.out.println(nomedois);
+         
+    
+     ArrayList<Double> nometres = new ArrayList<Double>();
+ 
+             nometres.add(6.5);
+             nometres.add(8.5);
+             nometres.add(4.5);
+             nometres.add(9.5);
+             nometres.add(61.5);
+     
+ 
+        System.out.println(nometres);   
+ 
+}
+}
