70d996b6df08796b94df7762958860c07acd9603
NTfabricio2000 <166646229+NTfabricio2000@users.noreply.github.com>
2024-05-02 18:17:31 -0300

    BoletimArrayaList.java

---
+++ BoletimArrayaList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExemploArraylist {
+     public static void main(String[] args) {
+    	 
+         Scanner sc = new Scanner(System.in);
+           Double str = sc.nextDouble();
+             
+             
+         
+   
+    	 ArrayList<String> nome = new ArrayList<String>();
+    	  String n;
+    	 nome.add(n = sc.next());
+  
+      ArrayList<Integer> idade = new ArrayList<Integer>();
+        Double i;
+        idade.add( i = sc.nextDouble());
+        	 
+    for(;;) { ArrayList<Double> nota  = new ArrayList<Double>();
+      Double nt;
+      nota.add( nt = sc.nextDouble());
+ 
+       
+    
+        ArrayList<Double> nota2  = new ArrayList<Double>();
+         Double n2;
+         nota2.add( n2 = sc.nextDouble());}
+
+    ArrayList<Double> media = new ArrayList<Double>(); 
+         Double m;
+             media.add(m = for(;;)
+        
+           System.out.println(nome, idade, note, nota2, media);
+          
